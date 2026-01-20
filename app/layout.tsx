3a04579844ad444c2a30a3f8e899f07ecbc11865
Venu Boga <bogavenugopal96@gmail.com>
2026-01-20 17:27:27 +0530

    Switch from Google Analytics to Google Tag Manager (GTM-NS3TCL8S)

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { getBaseUrl } from "@/config/site";
 import { fetchCategories, fetchSubcategories, fetchArticles, fetchAuthors, fetchTags } from "@/lib/api";
 import "./globals.css";
 
-const GA_MEASUREMENT_ID = "G-2W427S0D28";
+const GTM_ID = "GTM-NS3TCL8S";
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -65,25 +65,31 @@ export default async function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
       <head>
-        {/* Google Tag (gtag.js) */}
+        {/* Google Tag Manager */}
         <Script
-          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          strategy="afterInteractive"
-        />
-        <Script
-          id="gtag-init"
+          id="gtm-script"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
       <body className="antialiased font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SearchProvider
           articles={articles}
           authors={authors}
